8b14a063541c7253f9b1da8500b73f9140f49863
adsdesh <89637475+adsdesh@users.noreply.github.com>
2021-08-27 15:41:58 +0530

    Test

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("a is greater than b.");
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("b is greater than a.");
+            }
+            else
+            {
+                Console.WriteLine("Numbers are equal.");
+            }
+        }
+    }
+}
